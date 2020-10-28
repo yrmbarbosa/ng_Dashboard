5452b37a221adda542b0d6414380b3850a6d9693
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-28 17:48:39 -0300

    Implementando o serviço de dados

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
 
+  readonly dados = [
+    ['BMW', 40],
+    ['Audi', 30],
+    ['Mercedes', 90],
+    ['Jaguar', 20],
+    ['Volvo', 10]
+  ];
+
   constructor() { }
+
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete();
+    });
+  }
+
 }
